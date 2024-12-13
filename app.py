fff464560cd09efc7f3047ff450d12d095fed204
Aryan <132284891+aryanmangrule402@users.noreply.github.com>
2024-12-14 01:37:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('loading.html')  # Main content page
+
+@app.route('/home')
+def loading():
+    return render_template('home.html')  # Loading animation page
+
+@app.route('/adhyay14')
+def adhyay14():
+    return render_template('adhyay14.html')
+
+@app.route('/adhyay18')
+def adhyay18():
+    return render_template('adhyay18.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
